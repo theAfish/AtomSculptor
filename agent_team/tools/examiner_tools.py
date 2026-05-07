4feb509f3eed6396e26b5cc10976a90ba9484ec7
theAfish <taoyuze.lv@outlook.com>
2026-05-07 12:13:22 +0800

    feat: support atomworld bench calling

---
+++ agent_team/tools/examiner_tools.py
@@ -0,0 +1,10 @@
+# render structures
+# TODO: implement logic
+
+
+
+
+
+def draw_structure(structure_file):
+    # code to render structure from file, return rendered image or visualization
+    pass
